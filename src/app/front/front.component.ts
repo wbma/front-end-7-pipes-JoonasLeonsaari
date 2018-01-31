3d117607cf96cb8320a5da3a0d82f27c3ad238f3
Joonas Leonsaari <joonas.leonsaari@gmail.com>
2018-01-31 15:03:16 +0200

    task complete

---
+++ src/app/front/front.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-front',
+  templateUrl: './front.component.html',
+  styleUrls: ['./front.component.scss'],
+})
+export class FrontComponent implements OnInit {
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit() {
+    if (localStorage.getItem('token') === null) {
+      this.router.navigate(['login']);
+    }
+  }
+
+}
---
+++ src/app/services/media.service.ts
@@ -0,0 +1,73 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
+import {Router} from '@angular/router';
+
+@Injectable()
+export class MediaService {
+
+  apiUrl = 'http://media.mw.metropolia.fi/wbma';
+  username: string;
+  password: string;
+  status: string;
+  email: string;
+
+  constructor(public http: HttpClient, private router: Router) {
+  }
+
+  private formValidation(): boolean {
+    if (!this.username) {
+      alert('please check that all required fields have been filled');
+      return false;
+    } else if (!this.password) {
+      alert('please check that all required fields have been filled');
+      return false;
+    } else if (!this.email) {
+      alert('please check that all required fields have been filled');
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  public register() {
+    if (this.formValidation()) {
+      console.log('username' + this.username);
+      console.log('password' + this.password);
+      console.log('email' + this.email);
+
+      const body = {
+        username: this.username,
+        password: this.password,
+        email: this.email,
+      };
+      this.http.post(this.apiUrl + '/users', body).subscribe(data => {
+        console.log(data);
+        this.login();
+      });
+    }
+  }
+
+  public login() {
+    console.log('username: ' + this.username);
+    console.log('password: ' + this.password);
+
+    const body = {
+      username: this.username,
+      password: this.password,
+    };
+
+    const settings = {
+      headers: new HttpHeaders().set('Content-Type', 'application/json'),
+    };
+
+    this.http.post(this.apiUrl + '/login', body, settings).
+      subscribe(response => {
+        console.log(response['token']);
+        localStorage.setItem('token', response['token']);
+        this.router.navigate(['front']);
+      }, (error: HttpErrorResponse) => {
+        console.log(error.error.message);
+        this.status = error.error.message;
+      });
+  }
+}
